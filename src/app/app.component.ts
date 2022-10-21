999aeeb3b6e449b84aa58b695f976882301fa291
Guilherme <guilhermetarter@hotmail.com>
2022-10-21 12:53:25 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  template: `
+      <button class="button button-primary" (click)="subscribeToNotifications()">
+        Request notifications permission
+      </button>
+`})
 export class AppComponent {
-  title = 'notifications-tests';
+
+  readonly VAPID_PUBLIC_KEY = "BB6QW1_dhyh2Wz8pQUvJPA1PTZyw_yyS9X60clUa4xxNQ9ct_Jq8G-CGHclXIUI8z4XQVV-I5fl5GXcrkEKlQ30";
+
+  constructor(private swPush: SwPush) {}
+
+  subscribeToNotifications() {
+          this.swPush.requestSubscription({
+          serverPublicKey: this.VAPID_PUBLIC_KEY
+      })
+      .then(sub => { /* storeSubscriptionInTheBackend(sub) */ })
+      .catch(err => console.error("Could not subscribe to notifications", err));
+  }
 }
